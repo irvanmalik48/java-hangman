2f6fc70c5aa74f73872535d8c878599d2bb7de81
Irvan Malik <irvanmalik48@gmail.com>
2021-09-12 13:12:13 +0700

    repo: add quickstart script
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ compile.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "Checking if Java compiler is available..";
+
+if hash javac 2>/dev/null; then
+    echo "Compiling...";
+    javac App.java \
+        src/com/irvanma/Game.java \
+        src/com/irvanma/Variables.java \
+        src/com/irvanma/input/Input.java \
+        src/com/irvanma/levels/Levels.java;
+    echo "Bundling to JAR...";
+    jar cfe JavaHangman.jar App \
+        App.class \
+        src/com/irvanma \
+        src/com/irvanma/input \
+        src/com/irvanma/levels;
+    mkdir dist;
+    mv JavaHangman.jar dist/JavaHangman.jar;
+    echo "Deleting leftovers...";
+    rm -rf *.class \
+        src/com/irvanma/*.class \
+        src/com/irvanma/input/*.class \
+        src/com/irvanma/levels/*.class;
+    echo "Script succeeded.";
+else
+    echo "Please install Java JDK";
+fi
